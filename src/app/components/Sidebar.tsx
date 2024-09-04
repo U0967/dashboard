2f547b567218f665735d04572697675fe64ec2af
Uriel Lazaro <uriel.lazaro@outlook.com>
2024-09-03 23:15:43 -0600

    Creación de la plantilla de administración

---
+++ src/app/components/Sidebar.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image'
+import {IoBrowsersOutline, IoCalculator, IoLogoReact, IoUmbrella} from 'react-icons/io5';
+import SidebarMenuItem from './SidebarMenuItem';
+
+
+
+
+const menuItems = [
+    {
+        path: '/dashboard/main',
+        icon: <IoBrowsersOutline size={40} />,
+        title: 'Dashboard',
+        subTitle: 'Administración'
+    },
+    {
+        path: '/dashboard/counter',
+        icon: <IoCalculator size={40} />,
+        title: 'Counter',
+        subTitle: 'Contador Client Side'
+    },
+]
+
+const Sidebar = () => {
+  return (
+    <div id="menu" style={{width: '400px'}} className="bg-gray-900 min-h-screen z-10 text-slate-300 w-64 left-0 h-screen overflow-y-scroll">
+           <div id="logo" className="my-4 px-6">
+            <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
+                <IoLogoReact className='mr-2' />
+                <span>Destello</span> 
+                <span className="text-blue-500">Celestial</span>.
+            </h1>
+            <p className="text-slate-500 text-sm">Panel de adminitración</p>
+           </div>
+           <div id="profile" className="px-6 py-10">
+            <p className="text-slate-500">Bienvenido,</p>
+            <a href="#" className="inline-flex space-x-2 items-center">
+                <span>
+                    <Image className="rounded-full w-8 h-8" 
+                    src="https://images.unsplash.com/photo-1542909168-82c3e7fdca5c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=128&q=80" 
+                    alt="User avatar"
+                    width={50}
+                    height={50} />
+                </span>
+                <span className="text-sm md:text-base font-bold">
+                    Uriel Lázaro
+                </span>
+                </a>
+           </div>
+           <div id="nav" className="w-full px-6">
+
+            {
+                menuItems.map(item => (
+                    <SidebarMenuItem key={item.path} {...item}/>
+                ))
+            }
+
+           </div>
+        </div>
+  )
+}
+
+export default Sidebar
