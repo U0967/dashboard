268974fb252f673cbe3f43d8bcfdd7cbda3e24e3
Uriel Lazaro <uriel.lazaro@outlook.com>
2024-09-05 01:51:34 -0600

    Integración de la pantalla de agregar favoritos

---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,27 @@
+// procesar la data para obetenere y mostrar los resultados de los pokemones
+
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from 'next/image';
+
+//tipar la data inicia con promise
+const getPokemons = async (limit=20, offset=0):Promise<SimplePokemon[]> => {
+    const data:PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+    .then(res=> res.json());
+
+    const pokemons =data.results.map(pokemon =>({
+        id: pokemon.url.split('/').at(-2)!,
+        name: pokemon.name,
+    }))
+
+    return pokemons;
+}
+export default async function PokemonsPage() {
+
+    const pokemons = await getPokemons(151);
+  return (
+    <div className="flex flex-col">
+        <span className="text-5xl my-2">Listado de Pokémons <small>estático</small></span>
+      <PokemonGrid pokemons={pokemons}/>
+    </div>
+  );
+}
